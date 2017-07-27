14ad583e556ef1051be79322bde69840db684191
EIpescado <1724537432@qq.com>
2017-07-27 18:09:58 +0800

    sleuth and zipkin

--- simpleConsumer/src/main/java/org/skyCloud/simpleConsumer/controller/SimpleController.java
+++ simpleConsumer/src/main/java/org/skyCloud/simpleConsumer/controller/SimpleController.java
@@ -4,6 +4,7 @@ import org.skyCloud.simpleConsumer.client.SimpleClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,9 +20,12 @@ public class SimpleController {
 
     @Autowired
     private SimpleClient simpleClient;
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @RequestMapping(value = "/add" ,method = RequestMethod.GET)
     public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
+        logger.info("开始调用 {} 服务",discoveryClient.getLocalServiceInstance().getServiceId());
        return simpleClient.add(a,b);
     }
 
