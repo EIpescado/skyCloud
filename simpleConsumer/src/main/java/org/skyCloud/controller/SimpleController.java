586a4e1db38c041f2dac92f90321e3f1731b41b0
EIpescado <1724537432@qq.com>
2017-12-07 17:18:23 +0800

    update config

--- simpleConsumer/src/main/java/org/skyCloud/controller/SimpleController.java
+++ simpleConsumer/src/main/java/org/skyCloud/controller/SimpleController.java
@@ -1,10 +1,13 @@
 package org.skyCloud.controller;
 
 import org.skyCloud.client.SimpleClient;
+import org.skyCloud.common.dataWrapper.BackResult;
+import org.skyCloud.stream.SimpleSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +25,16 @@ public class SimpleController {
     private SimpleClient simpleClient;
     @Autowired
     private DiscoveryClient discoveryClient;
+    @Autowired
+    private SimpleSource simpleSource;
+
+    @GetMapping("processMessage")
+    public void processEvent() {
+        simpleSource.simpleConsumer_system_output().send(
+//                MessageBuilder.withPayload("{\"code\":\"1\",\"msg\":\"成功\",\"data\":[{\"code\":\"\",\"name\":\"全部\",\"tote\":74953,\"documentStatusId\":\"\"}]}")
+                MessageBuilder.withPayload(new BackResult())
+                        .build());
+    }
 
     @GetMapping("listMenu")
     public Object listMenu() {
